feb8c9f22dbcb6fdbc7c16160ca80d956587ac20
Moulishwaran <moulishwaran6010@gmail.com>
2024-08-13 00:55:20 +0100

    Added transaction database with income and expenses and setup for income and expenses views are done

--- expense_tracker/expense_tracker/expense_tracker_app/models.py
+++ expense_tracker/expense_tracker/expense_tracker_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    #transaction type tuple to diffrentiate income and expense
+    TRANSACTION_TYPES = (
+        ('income','Income'),
+        ('expense','Expense'),
+    )
+
+    #amount variable to store the expense and income
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    #type of the transaction determined from TRANSACTION_TYPES
+    type = models.CharField(max_length=7,choices=TRANSACTION_TYPES)
+    #category of the transaction like Food,Transport
+    category = models.CharField(max_length=50)
+    #description of the transaction
+    description = models.TextField(blank=True,null=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.get_type_display()} - {self.category}: ${self.amount}"
+
+
--- expense_tracker/expense_tracker/expense_tracker_app/views.py
+++ expense_tracker/expense_tracker/expense_tracker_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+#getting the Transaction Database from models.py
+from .models import Transaction
+
+def income_list(request):
+    #filters transactions to show only income
+    incomes = Transaction.objects.filter(type='income')
+    return render(request, 'transactions/income_list.html',{'transactions': incomes})
+
+def expense_list(request):
+    #filters transactions to show only expenses
+    expenses = Transaction.objects.filter(type='expense')
+    return render(request, 'transactions/expense_list.html', {'transaction': expenses})
+
+
