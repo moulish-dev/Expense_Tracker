ad6c722db619b11d29ea95fda11932309f785c61
Moulishwaran <moulishwaran6010@gmail.com>
2024-08-13 21:51:57 +0100

    Add all transactions home page and a transaction form to add input

--- expense_tracker/expense_tracker/expense_tracker_app/urls.py
+++ expense_tracker/expense_tracker/expense_tracker_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('income/', views.income_list, name='income_list'),
     path('expense/',views.expense_list, name='expense_list'),
+    path('transactions/',views.transactions_list, name='transaction_list'),
+    path('add/', views.add_transaction, name='add_transaction'),
 ]
--- expense_tracker/expense_tracker/expense_tracker_app/views.py
+++ expense_tracker/expense_tracker/expense_tracker_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 #getting the Transaction Database from models.py
 from .models import Transaction
+
+from .forms import TransactionForm
 
 def income_list(request):
     #filters transactions to show only income
@@ -11,6 +13,19 @@ def income_list(request):
 def expense_list(request):
     #filters transactions to show only expenses
     expenses = Transaction.objects.filter(type='expense')
-    return render(request, 'transactions/expense_list.html', {'transaction': expenses})
+    return render(request, 'transactions/expense_list.html', {'transactions': expenses})
 
+def transactions_list(request):
+    #shows all transactions
+    transactions = Transaction.objects.all().order_by('date')
+    return render(request, 'transactions/transaction_list.html',{'transactions': transactions})
 
+def add_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm()
+    return render(request, 'transactions/add_transaction.html', {'form': form})
