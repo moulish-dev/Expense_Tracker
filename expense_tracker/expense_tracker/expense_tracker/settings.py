466cab169f4a13d75ec947b24a027b2ef55a8bca
Moulishwaran <moulishwaran6010@gmail.com>
2024-09-22 10:14:50 +0100

    Changed debug function

--- expense_tracker/expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/expense_tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kdzd(s0hb)imy#b1zb*f!lyfq^i9_a@_1_(sk)v0^ms8#7hb92'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
