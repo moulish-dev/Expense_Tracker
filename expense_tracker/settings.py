11c0b4a10282780e7cb68f880327173b12e6fe5c
Moulishwaran <moulishwaran6010@gmail.com>
2024-09-24 16:50:28 +0100

    changed the deployment files to serve static files

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -130,7 +131,7 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = ''
 LOGOUT_REDIRECT_URL = 'logout'
 
-STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'expense_tracker_app/static')]
 # For storing static files
