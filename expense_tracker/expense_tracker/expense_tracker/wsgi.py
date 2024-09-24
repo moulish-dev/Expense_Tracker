103e522805c265e5c90c4fe7bf6d1997b8fc737d
Moulishwaran <moulishwaran6010@gmail.com>
2024-09-24 13:53:56 +0100

    debugging module import issue

--- expense_tracker/expense_tracker/expense_tracker/wsgi.py
+++ expense_tracker/expense_tracker/expense_tracker/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+print("Current working directory:", os.getcwd())
+print("Python Path:", sys.path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expense_tracker.settings')
 
