a28cf7f5a223e60d55087ce2ba1ca6cdec74e94f
Moulishwaran <moulishwaran6010@gmail.com>
2024-09-24 19:21:02 +0100

    added appname before staticfiles

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware", #WhiteNoise Middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'expense_tracker_app/static' ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'expense_tracker_app/static/'
+
 
 
 # Default primary key field type
@@ -141,4 +139,3 @@ LOGOUT_REDIRECT_URL = 'logout'
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'expense_tracker_app/static')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
