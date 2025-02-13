eb4b49dc1c1c471786446abc84bd730909b98664
Moulishwaran Balaji <moulish.developer@gmail.com>
2025-02-13 21:07:51 +0000

    fixed environment key problem

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -14,12 +14,21 @@ from pathlib import Path
 import os
 import environ
 
-env = environ.Env()
 
-# Determine which environment file to load
-ENV_FILE = os.getenv('DJANGO_ENV_FILE', '.env')  # Default is `.env`
 
-environ.Env.read_env(ENV_FILE)  # Load the correct .env file
+# Initialize environ
+env = environ.Env(
+    DJANGO_SECRET_KEY=(str, 'fallback-secret-key')  # Default value
+)
+
+# Read .env file (if exists)
+environ.Env.read_env(os.getenv("DJANGO_ENV_FILE", ".env"))
+
+# Get the secret key
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,8 +37,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
